e1a34fc9bebfb8a21d7825fe72c17e03219dfd5e
cerimice <cerimice@gmail.com>
2020-07-25 08:48:40 -0500

    Primer Commit - Prueba

--- src/main/java/com/tiamex/siicomeii/Main.java
+++ src/main/java/com/tiamex/siicomeii/Main.java
@@ -4,5 +4,6 @@ package com.tiamex.siicomeii;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
 }
